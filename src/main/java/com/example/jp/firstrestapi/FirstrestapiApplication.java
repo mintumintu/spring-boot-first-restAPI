492d4c1f6b7a593496bb4295d61e6db784b50705
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2023-02-03 07:11:30 +0530

    initial Commit

---
+++ src/main/java/com/example/jp/firstrestapi/FirstrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jp.firstrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jp/firstrestapi/FirstrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jp.firstrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
